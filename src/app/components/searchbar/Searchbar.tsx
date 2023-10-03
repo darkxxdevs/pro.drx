2a634cc9266dbf58ed7f162fa6d371ee24371510
arpit <arpit2003y@gmail.com>
2023-10-03 11:35:45 +0530

    added requests configs

--- src/app/components/searchbar/Searchbar.tsx
+++ src/app/components/searchbar/Searchbar.tsx
@@ -1,17 +1,17 @@
 "use client";
-import React, { FormEvent } from "react";
-import { useState } from "react";
+import React, { FormEvent, useState } from "react";
+import { scrapeAndStoreProduct } from "../../../../lib/actions";
 
 const Searchbar = () => {
-  const [searchPompt, setSearchPrompt] = useState("");
-  const [isLoading, setisLoading] = useState(false);
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const isValidAmazonProductURL = (url: string) => {
     try {
       const parsedUrl = new URL(url);
       const hostname = parsedUrl.hostname;
 
-      //-> check if  hostname contains amazon.com or amazon.country
+      // Check if the hostname contains amazon.com or amazon.country
       if (
         hostname.includes("amazon.com") ||
         hostname.includes("amazon.") ||
@@ -24,34 +24,41 @@ const Searchbar = () => {
     }
   };
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const isValidLink = isValidAmazonProductURL(searchPompt);
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-    if (!isValidLink) return alert("please provide a valid amazon link !");
+    if (!isValidLink) {
+      return alert("Please provide a valid Amazon link!");
+    }
 
     try {
-      setisLoading(true);
+      setIsLoading(true);
+
+      // scrape the product page
+      const product = await scrapeAndStoreProduct(searchPrompt);
     } catch (error) {
+      console.log(error);
     } finally {
-      setisLoading(false);
+      setIsLoading(false);
     }
   };
+
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
-        value={searchPompt}
+        value={searchPrompt}
         placeholder="Enter product link"
-        className=" w-[60%] md:w-[80%] border-2 border-slate-100 bg-transparent px-5 py-5 rounded-lg outline-0"
+        className="w-[60%] md:w-[80%] border-2 border-slate-100 bg-transparent px-5 py-5 rounded-lg outline-0"
         onChange={(e) => {
           setSearchPrompt(e.target.value);
         }}
       />
       <button
         type="submit"
-        className=" text-white bg-black px-5 py-2 rounded-lg font-bold"
-        disabled={searchPompt === " "}
+        className="text-white bg-black px-5 py-2 rounded-lg font-bold"
+        disabled={!searchPrompt.trim()} // Disable when searchPrompt is empty or only contains whitespace
       >
         {isLoading ? "Searching...." : "Search"}
       </button>
